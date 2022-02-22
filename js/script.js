624cbb5835b9342153a221c9867c14b305dfa38b
Avril_M <turvey.am@gmail.com>
2022-02-22 10:46:09 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector("input");
+const buttonChange = document.querySelector("input.change");
+
+button.addEventListener("mouseover", function () {
+    button.classList.add("change");
+});
+
